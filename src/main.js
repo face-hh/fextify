f0cce5a8f6ec386b3a3d1782e7bb7a6380cb3ddd
Kovács Bálint Hunor <kovacsbalinthunor53@gmail.com>
2023-11-13 09:14:42 +0200

    fix(main): fixed language specific characters for a subset of languages
    Note: with this change you can write in the title and in the editor language specific characters
    
    Example: I can write "hálás"  ( a word in Hungarian meaning grateful )
    Or I can write the word "recunoscător" ( which means the same in Romanian ) in the title or in the editor
    
    I even tested it with Anthy Japanese and it works great

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-const textarea = document.getElementById('main');
-const display = document.getElementById('textDisplay');
+const textarea = document.getElementById("main");
+const display = document.getElementById("textDisplay");
 
 const cursor = document.getElementById("cursor");
 
@@ -8,43 +8,67 @@ update_active(tabs_.length);
 
 let info = await retrieve_last_opened();
 
-if (info[0] === 'None') info[0] = '', info[1] = ''
+if (info[0] === "None") (info[0] = ""), (info[1] = "");
 
-window.title = $('#title');
-window.main = $('#main');
-window.themeInput = $('#themeInput');
-window.css = $('#cssStyling');
+window.title = $("#title");
+window.main = $("#main");
+window.themeInput = $("#themeInput");
+window.css = $("#cssStyling");
 
-const popup = $('#popup');
+const popup = $("#popup");
 
 let theme = await get_theme();
-if(theme) changeTheme(theme), window.themeInput.val(theme);
+if (theme) changeTheme(theme), window.themeInput.val(theme);
 
-window.path = info[0]
+window.path = info[0];
 update_words(info[1]);
 
 autosize(window.title);
 
-window.editor = await BalloonEditor
-  .create(document.querySelector("#main"),
-    {
-      extraPlugins: ['Markdown']
-    })
-  .catch(error => {
-    console.error(error);
-  });
+// Language support
+const supportedLanguages = [
+  "en",
+  "fr",
+  "de",
+  "es",
+  "pt",
+  "ru",
+  "zh-cn",
+  "ja",
+  "ko",
+  "pl",
+  "hu",
+  "ro",
+];
 
-window.title.val(window.path)
-editor.setData(info[1])
+// Get the user's browser language or use a default language
+const userLanguage = navigator.language || "en";
 
-autosize.update(title)
+// Check if the user's language is in the list of supported languages
+const selectedLanguage = supportedLanguages.includes(userLanguage)
+  ? userLanguage
+  : "en";
 
-window.title.on('input', async () => {
-  save_title(window.path, window.title.val());
+window.editor = await BalloonEditor.create(document.querySelector("#main"), {
+  extraPlugins: ["Markdown"],
+  language: selectedLanguage,
+}).catch((error) => {
+  console.error(error);
+});
+
+window.title.val(decodeURIComponent(info[0]));
+editor.setData(info[1]);
+
+autosize.update(title);
+
+window.title.addEventListener("input", async (event) => {
+  const encodedTitle = encodeURIComponent(window.title.val());
+
+  save_title(window.path, encodedTitle);
   manage_tabs(await get_tabs());
 
-  window.path = window.title.val();
-})
+  window.path = encodedTitle;
+});
 
 /* ***** */
 const observer = new MutationObserver(function (mutations) {
@@ -58,15 +82,20 @@ const observer = new MutationObserver(function (mutations) {
   });
 });
 
-observer.observe(textarea, { childList: true, characterData: true, subtree: true });
+observer.observe(textarea, {
+  childList: true,
+  characterData: true,
+  subtree: true,
+});
 
 /* ***** */
 
 window.title.keydown(function (e) {
-  if (!/[a-zA-Z0-9]| |\:|\!|\"|\'|\,|\./.test(e.key) || e.key == "Enter") return e.preventDefault();
+  if (!/[a-zA-Z0-9]| |\:|\!|\"|\'|\,|\./.test(e.key) || e.key == "Enter")
+    return e.preventDefault();
 });
 
-$('.pages').on('click', (event) => {
+$(".pages").on("click", (event) => {
   const target = event.target;
 
   if (event.ctrlKey) return delete_tab(target);
@@ -74,89 +103,89 @@ $('.pages').on('click', (event) => {
   switch_tab(target);
 });
 
-const isMac = navigator.userAgent.toLowerCase().includes('mac');
+const isMac = navigator.userAgent.toLowerCase().includes("mac");
 
-themeInput.keydown(e => {
-  if(e.key === "Enter"){
-    changeTheme(themeInput.val())
+themeInput.keydown((e) => {
+  if (e.key === "Enter") {
+    changeTheme(themeInput.val());
   }
-}) 
+});
 
-$('body').keydown(async e => {
+$("body").keydown(async (e) => {
   const ctrlKey = isMac ? e.metaKey : e.ctrlKey;
 
-  if (e.key === 'p' && ctrlKey) {
+  if (e.key === "p" && ctrlKey) {
     e.preventDefault();
 
     animateDiv(undefined, popup); // handle command pallet
   }
 
-  if (e.key === 'Escape') {
+  if (e.key === "Escape") {
     e.preventDefault();
 
     animateDiv(true, popup); // force exit command pallet
-    animateDiv(true, $('#cssStyling')); // force exit themes
+    animateDiv(true, $("#cssStyling")); // force exit themes
   }
 
-  if (e.key === 'w' && ctrlKey) {
+  if (e.key === "w" && ctrlKey) {
     e.preventDefault();
 
-    handleCommandPrompt('Close current file');
+    handleCommandPrompt("Close current file");
   }
   if (/^[1-9]*$/.test(e.key) && ctrlKey) {
     e.preventDefault();
 
-    handleCommandPrompt('Switch file', e.key);
+    handleCommandPrompt("Switch file", e.key);
   }
-  if (e.key === 'n' && ctrlKey) {
+  if (e.key === "n" && ctrlKey) {
     e.preventDefault();
 
-    handleCommandPrompt('New file');
+    handleCommandPrompt("New file");
   }
-  if (e.key === 'o' && ctrlKey) {
+  if (e.key === "o" && ctrlKey) {
     e.preventDefault();
 
     const arg = await ask_for_file();
 
-    handleCommandPrompt('Open file', arg);
+    handleCommandPrompt("Open file", arg);
   }
 
-  if (e.key === 'H' && ctrlKey && e.shiftKey) {
+  if (e.key === "H" && ctrlKey && e.shiftKey) {
     e.preventDefault();
 
-    handleCommandPrompt('Copy HTML output');
+    handleCommandPrompt("Copy HTML output");
   }
 
-  if (e.key === 'Tab' && ctrlKey) {
+  if (e.key === "Tab" && ctrlKey) {
     e.preventDefault();
 
-    handleCommandPrompt('Switch file (quick)');
+    handleCommandPrompt("Switch file (quick)");
   }
-  if (e.code === 'KeyS' && ctrlKey && e.altKey) {
+  if (e.code === "KeyS" && ctrlKey && e.altKey) {
     e.preventDefault();
 
     animateDiv(undefined, css);
   }
 
-  const focused = document.querySelector('.focused');
+  const focused = document.querySelector(".focused");
 
-  if (e.key == 'Enter' && focused) {
-    const suggestion = focused.querySelector('.left-content span');
+  if (e.key == "Enter" && focused) {
+    const suggestion = focused.querySelector(".left-content span");
     const content = suggestion.textContent;
 
-    if (content === 'Switch file') {
+    if (content === "Switch file") {
       handleCommandPrompt(content, 1); // default to switch to 1st tab
     } else handleCommandPrompt(content);
   }
-})
+});
 
 window.animateDiv = async function (forceClose, el) {
-  let isDisabled = el.prop('disabled');
+  let isDisabled = el.prop("disabled");
 
   if (isDisabled === undefined) isDisabled = true;
   if (forceClose) isDisabled = false;
 
-  const fadeAction = isDisabled ? 'fadeIn' : 'fadeOut'
+  const fadeAction = isDisabled ? "fadeIn" : "fadeOut";
 
   const windowHeight = $(window).height();
   const popupHeight = el.outerHeight();
@@ -165,72 +194,76 @@ window.animateDiv = async function (forceClose, el) {
 
   const topPosition = scrollPosition + (windowHeight - popupHeight) / 2;
 
-  el.css('top', topPosition);
+  el.css("top", topPosition);
 
   el[fadeAction](250, function () {
-    el.prop('disabled', !isDisabled);
+    el.prop("disabled", !isDisabled);
 
     if (isDisabled) {
-      $('.input').focus();
-      $('body').css('overflow-y', 'hidden');
+      $(".input").focus();
+      $("body").css("overflow-y", "hidden");
     } else {
       window.main.focus();
-      $('body').css('overflow-y', 'auto');
+      $("body").css("overflow-y", "auto");
     }
   });
-}
+};
 
-document.addEventListener('keydown', handleArrowNavigation);
+document.addEventListener("keydown", handleArrowNavigation);
 
 function handleArrowNavigation(event) {
-  !popup.prop('disabled') && ['ArrowUp', 'ArrowDown'].includes(event.key) ? event.preventDefault() : '';
+  !popup.prop("disabled") && ["ArrowUp", "ArrowDown"].includes(event.key)
+    ? event.preventDefault()
+    : "";
 
-  const navigableDivs = document.querySelectorAll('.suggestion');
+  const navigableDivs = document.querySelectorAll(".suggestion");
   let currentIndex = -1;
 
   for (let i = 0; i < navigableDivs.length; i++) {
-    if (navigableDivs[i].classList.contains('focused')) {
+    if (navigableDivs[i].classList.contains("focused")) {
       currentIndex = i;
-      navigableDivs[i].classList.remove('focused');
+      navigableDivs[i].classList.remove("focused");
     }
   }
 
   let nextIndex;
 
   switch (event.key) {
-    case 'ArrowUp':
-      nextIndex = currentIndex > 0 ? currentIndex - 1 : navigableDivs.length - 1;
+    case "ArrowUp":
+      nextIndex =
+        currentIndex > 0 ? currentIndex - 1 : navigableDivs.length - 1;
       break;
 
-    case 'ArrowDown':
-      nextIndex = currentIndex < navigableDivs.length - 1 ? currentIndex + 1 : 0;
+    case "ArrowDown":
+      nextIndex =
+        currentIndex < navigableDivs.length - 1 ? currentIndex + 1 : 0;
       break;
 
     default:
       return;
   }
 
-  navigableDivs[nextIndex].classList.add('focused');
+  navigableDivs[nextIndex].classList.add("focused");
 }
 
 const options = {
-  valueNames: ['left-content'],
+  valueNames: ["left-content"],
   fuzzySearch: {
-    searchClass: 'fuzzy-search',
+    searchClass: "fuzzy-search",
     location: 0,
     distance: 100,
     threshold: 0.4,
-    multiSearch: true
-  }
+    multiSearch: true,
+  },
 };
 
-const sl = new List('suggestionContainer', options);
-const sc = document.getElementById('suggestionContainer');
-const list = document.getElementById('suggestionList');
+const sl = new List("suggestionContainer", options);
+const sc = document.getElementById("suggestionContainer");
+const list = document.getElementById("suggestionList");
 
-const searchInput = document.getElementById('searchInput');
+const searchInput = document.getElementById("searchInput");
 
-searchInput.addEventListener('input', (event) => {
+searchInput.addEventListener("input", (event) => {
   const query = event.target.value;
 
   sl.fuzzySearch(query);
@@ -238,11 +271,10 @@ searchInput.addEventListener('input', (event) => {
   updatePromptHeight();
 });
 
-
 function updatePromptHeight() {
   const newHeight = Math.min(list.children.length * 35, 500);
 
-  $(sc).animate({ height: `${newHeight}px` }, 100)
+  $(sc).animate({ height: `${newHeight}px` }, 100);
 }
 
 updatePromptHeight();
